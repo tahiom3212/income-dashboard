524efaf76320aab5a5a44cd05b6649fbd8fc8342
tahiom3212 <tahiom3212@gmail.com>
2026-04-05 10:29:05 +0200

    Create income_tracker_script.py
    First version of my Python income tracker

---
+++ income_tracker_script.py
@@ -0,0 +1,18 @@
+name = "Igor"
+country = "Sweden"
+goal = 50000
+current_income = 10000
+
+progress = (current_income / goal) * 100
+
+print("Привіт,", name)
+print("Країна:", country)
+print("Ціль по доходу:", goal, "euro")
+print("Поточний дохід:", current_income, "euro")
+print(f"Ти виконав {progress:.2f}% від своєї цілі")
+
+if current_income >= goal:
+    print("🎉 Вітаю! Ти досяг своєї цілі!")
+else:
+    remaining = goal - current_income
+    print(f"⚡ Ти ще можеш заробити {remaining} euro, щоб досягти мети")
